bb190c256ac4ef83577ea9de95b656e6a3085e0e
Lesko <matevzlesko@gmail.com>
2025-04-02 19:54:52 +0200

    Bump version 0.0.18 -> 0.0.19.

--- docker_release.bat
+++ docker_release.bat
@@ -1,10 +1,10 @@
 docker image ls
 
-docker build -t leskoiam/kronoterm_cloud_relay:0.0.17 .
+docker build -t leskoiam/kronoterm_cloud_relay:0.0.19 .
 docker image ls
 
-docker tag leskoiam/kronoterm_cloud_relay:0.0.17 leskoiam/kronoterm_cloud_relay:latest
+docker tag leskoiam/kronoterm_cloud_relay:0.0.19 leskoiam/kronoterm_cloud_relay:latest
 docker image ls
 
-docker push leskoiam/kronoterm_cloud_relay:0.0.17
+docker push leskoiam/kronoterm_cloud_relay:0.0.19
 docker push leskoiam/kronoterm_cloud_relay:latest
